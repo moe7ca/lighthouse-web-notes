e6010852fae2b8824250a08f84c0e2b44129e0b0
Ahmed Noori <moe7ca@gmail.com>
2022-02-24 04:15:12 -0500

    Create reverse.js

--- Week_1/Day_2/reverse.js
+++ Week_1/Day_2/reverse.js
@@ -3,6 +3,6 @@ var input = process.argv[2];
 if (input)
   console.log(reverse(input));
 
-function reverse(original) {
+const reverse = function(original) {
   return original.split('').reverse().join('');
-}
+};
